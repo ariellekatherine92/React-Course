5bbcce2c372ef45dff7ecfe19347e0574cb5f53c
“ariellekatherine92” <ariellekatherine92@gmail.com>
2021-07-21 16:18:53 -0400

    added props and components

--- react-course/src/App.js
+++ react-course/src/App.js
@@ -1,8 +1,13 @@
-
+import Todo from './components/Todo';
 
 function App() {
   return (
-    <div> Hello!</div>
+    <div>
+      <h1>My Todos</h1>
+      <Todo text='Learn React' />
+      <Todo text='Master React' />
+      <Todo text= 'Explore full React Course '/>
+    </div>
   );
 }
 
